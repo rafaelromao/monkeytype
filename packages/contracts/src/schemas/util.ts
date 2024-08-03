6c5eeadb8c28c97a884ebd4e46f3e262b28e5eb6
Christian Fehmer <cfe@sexy-developer.com>
2024-08-03 14:38:51 +0200

    frontend

--- packages/contracts/src/schemas/util.ts
+++ packages/contracts/src/schemas/util.ts
@@ -1,12 +1,8 @@
 import { z, ZodString } from "zod";
 
 export const StringNumberSchema = z
-
-  .custom<`${number}`>((val) => {
-    if (typeof val === "number") val = val.toString();
-    return typeof val === "string" ? /^\d+$/.test(val) : false;
-  }, "Needs to be a number represented as a string.")
-  .transform(String)
+  .string()
+  .regex(/^\d+$/)
   .describe('number wrapped as string, e.g. "10"');
 
 export type StringNumber = z.infer<typeof StringNumberSchema>;
